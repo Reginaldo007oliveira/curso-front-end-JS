2565baef3b46258b70808e80fbe067338e6952d8
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2025-04-10 12:00:37 -0300

    camelCase

--- variaveis.js
+++ variaveis.js
@@ -1,5 +1,4 @@
 // biome-ignore lint/style/useConst: <explanation>
-let name = "Luciano"
-var nam3 = "Nem"
-const neva ="nave"
-console.log(name, nam3, neva)
+const nome = "Luciano"
+const lastName = "bora"
+console.log(nome, lastName)
