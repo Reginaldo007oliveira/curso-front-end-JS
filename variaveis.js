87470648160c643b2c1a59aaa71bf5ce7f8577a5
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2025-04-10 12:23:59 -0300

    Arrays console

--- variaveis.js
+++ variaveis.js
@@ -4,5 +4,8 @@ const lastName = "bora"
 const idade = 57
 const peso = 2.10
 const altura = 3.20
+const frutas = ["banana", "maça", "mamão", 20]
 console.log(` A soma do peso mais a altura é ${peso + altura }...`)
 console.log(`Olá, ${nome} ${lastName} ${idade} ${peso} é minha idade`)
+console.log("frutas", frutas)
+console.log(frutas[2])
