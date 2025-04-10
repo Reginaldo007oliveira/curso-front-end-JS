519ec72eadd706336e760a1854748e2afbe067f5
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2025-04-10 18:25:03 -0300

    Pegar elemento pela tag

--- DOM-selecao/js/scripts.js
+++ DOM-selecao/js/scripts.js
@@ -1,9 +1,9 @@
-//seleção por ID
+// //seleção por ID
 const content = document.getElementById("content")
-// console.log("Contente", content)
-content.innerHTML=` <p> Parágrafo de textocc </p>`;
-content.style.backgroundColor="#f00"
-content.style.fontSize = "30px"
+// // console.log("Contente", content)
+// content.innerHTML=` <p> Parágrafo de textocc </p>`;
+// content.style.backgroundColor="#f00"
+// content.style.fontSize = "30px"
 
 //seleção por classe
 const classText = document.getElementsByClassName("text")
@@ -11,4 +11,11 @@ const classText = document.getElementsByClassName("text")
  
  for(text of classText){
     text.style.color = "blue"
+ }
+
+ //seleção por tag
+ const titles = document.getElementsByTagName("h2")
+ console.log("Titles", titles)
+ for(title of titles){
+    title.style.fontSize = "66px"
  }
