a073c27573f1241822c593d2b35de8534465b164
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2025-04-10 14:56:57 -0300

    key

--- 10_for-of-for-in.js
+++ 10_for-of-for-in.js
@@ -1,5 +1,10 @@
 let frutas = ["banana","mamao","abacate","pessego","limao"]
 
-for( let fruta of frutas){
-    console.log(fruta)
+// for( let fruta of frutas){
+//     console.log(fruta)
+// }
+
+let pessoa = {nome:"João", idade:55, genero:"masculino"}
+for(let key in pessoa){
+    console.log(key + " " + pessoa[key])
 }
