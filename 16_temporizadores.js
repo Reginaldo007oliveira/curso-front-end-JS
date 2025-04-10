1269426d5c35875aaac52df6656fe5bc1d321df4
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2025-04-10 16:18:34 -0300

    Temporizadores setInterval e setTimeout

---
+++ 16_temporizadores.js
@@ -0,0 +1,11 @@
+//setTimeout e SetInterval
+
+console.log("O resultado de 5 + 5 é")
+
+function resultado(){
+    console.log("10")
+}
+
+// setTimeout(resultado, 6000)
+
+setInterval(resultado, 300)
