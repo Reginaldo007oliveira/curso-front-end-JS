e6e0f57b9d6b962d0dafa192ddbd56c0a98ebfd5
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2025-04-10 13:56:07 -0300

    operadores matematicos

---
+++ 6_funcoes.js
@@ -0,0 +1,11 @@
+const goodMorning =(name) => {
+    return `Bom dia ${name} `
+}
+console.log(goodMorning("Luciano"))
+
+const adicao = (x,y) => x+y;
+const subtracao = (x,y) => x-y;
+const divisao = (x,y) => x/y;
+const multiplicacao = (x,y) => x*y;
+
+console.log(`resultado:${multiplicacao(12,140)}`)
