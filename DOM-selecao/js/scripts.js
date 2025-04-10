7666a8d6d419eb6370a6ad53d142a40f587948bb
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2025-04-10 18:12:36 -0300

    For of e getElementsByClassName

--- DOM-selecao/js/scripts.js
+++ DOM-selecao/js/scripts.js
@@ -1,6 +1,14 @@
 //seleção por ID
 const content = document.getElementById("content")
 // console.log("Contente", content)
-content.innerHTML=` <p> Parágrafo de texto</p>`
+content.innerHTML=` <p> Parágrafo de textocc </p>`;
 content.style.backgroundColor="#f00"
 content.style.fontSize = "30px"
+
+//seleção por classe
+const classText = document.getElementsByClassName("text")
+ console.log("ClassText", classText)  
+ 
+ for(text of classText){
+    text.style.color = "blue"
+ }
