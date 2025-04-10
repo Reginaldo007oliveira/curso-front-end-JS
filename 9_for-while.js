3089f803211af89ebf5401fd811d86199d872c9b
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2025-04-10 14:40:10 -0300

    for com array

--- 9_for-while.js
+++ 9_for-while.js
@@ -1,8 +1,13 @@
 // for (let i=0; i<=10; i++){
 //     console.log(i);
 // }
-let i=0;
-while (i<=10) {
-    console.log(i);
-    i++
-}
+
+// let i=0;
+// while (i<=10) {
+//     console.log(i);
+//     i++
+// }
+
+let frutas = ["banana","maça","mamão"]
+       for(let i=0; i<frutas.length; i++)
+        console.log(frutas[i])
