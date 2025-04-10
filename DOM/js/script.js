f99daf84089772eba0add051078bf6b3080a3ed3
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2025-04-10 17:21:09 -0300

    Alert incorportado Dom JS

---
+++ DOM/js/script.js
@@ -0,0 +1,4 @@
+const btabreAlertVinculado = document.querySelector("#abreAlertVinculado")
+btabreAlertVinculado.addEventListener("click", () => {
+    alert("Abrindo janela pelo script vinculado")
+})
