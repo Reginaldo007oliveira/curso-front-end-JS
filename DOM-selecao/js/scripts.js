b188fdf56626000e4d663e0526d265e3b76c2bb5
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2025-04-10 17:40:25 -0300

    Pegando elemento e manipulando os campos com o js

---
+++ DOM-selecao/js/scripts.js
@@ -0,0 +1,6 @@
+//seleção por ID
+const content = document.getElementById("content")
+// console.log("Contente", content)
+content.innerHTML=` <p> Parágrafo de texto</p>`
+content.style.backgroundColor="#f00"
+content.style.fontSize = "30px"
