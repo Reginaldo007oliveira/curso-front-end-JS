c025398666191c6998b405220c7ec013f194cd11
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2025-04-10 13:45:18 -0300

    function

--- 5_oper_logic.js
+++ 5_oper_logic.js
@@ -1,4 +1,4 @@
-const age = 20
-const gender = "M"
-const result = age >= 18 && gender === "M"
+const age = 13
+const gender = "F"
+const result = age >= 18 || gender === "M"
 console.log(`${result}`)
