a2082244837b3ad6d7ebcdd074c92ad1d53560c7
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2025-04-10 12:29:45 -0300

    criação de objeto

--- variaveis.js
+++ variaveis.js
@@ -1,11 +1,21 @@
 // biome-ignore lint/style/useConst: <explanation>
+//dados estruturados
 const nome = "Luciano"
 const lastName = "bora"
 const idade = 57
 const peso = 2.10
 const altura = 3.20
 const frutas = ["banana", "maça", "mamão", 20]
+
+const usuario = {
+    nome: "Luciano",
+    lastName: "bora bora",
+    idade: 57,
+    
+}
 console.log(` A soma do peso mais a altura é ${peso + altura }...`)
 console.log(`Olá, ${nome} ${lastName} ${idade} ${peso} é minha idade`)
 console.log("frutas", frutas)
 console.log(frutas[2])
+console.log(usuario.lastName)
+console.log(usuario)
