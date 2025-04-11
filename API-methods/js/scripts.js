b693a28563319e606c5cf3ceb9753c0e4c374fbd
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2025-04-11 17:11:41 -0300

    Post API

--- API-methods/js/scripts.js
+++ API-methods/js/scripts.js
@@ -1,6 +1,30 @@
-//Fetch API
-fetch("https://jsonplaceholder.typicode.com/posts")
-	.then((response) => response.json())
-	.then((post) => {
-		console.log(post);
-	});
+//Fetch API Requisição do tipo GET
+// fetch("https://jsonplaceholder.typicode.com/posts")
+// 	.then((response) => response.json())
+	
+//     .then((post) => {
+// 		console.log(post);
+//         const container = document.querySelector("#container")
+
+//         post.map(post=>{
+//             console.log(post.title)
+
+//             const h2 = document.createElement("h2")
+//             h2.textContent = post.title
+//             container.appendChild(h2)
+//         })
+// 	});
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      title: 'foo',
+      body: 'bar',
+      userId: 1,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
