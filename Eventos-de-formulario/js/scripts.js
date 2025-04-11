58139fa61a288fbce8adac57ea34da76aa563f27
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2025-04-11 13:21:31 -0300

    Eventos de teclado

---
+++ Eventos-de-formulario/js/scripts.js
@@ -0,0 +1,31 @@
+const inputForm = document.querySelector("#busca");
+const botaoBusca = document.querySelector("#botaoBusca");
+const resultadoDiv = document.querySelector("#resultado");
+// const valorBusca = document.querySelector("#resultado");
+//evento de teclado key mostra a tecla que vc precionou
+inputForm.addEventListener("keydown", (e) => {
+	console.log(e.key);
+});
+
+inputForm.addEventListener("focus", (e) => {
+	inputForm.style.backgroundColor = "#FF0";
+});
+
+inputForm.addEventListener("blur", (e) => {
+	inputForm.style.backgroundColor = "";
+});
+
+function searchResult() {
+	const valorBusca = inputForm.value.trim();
+	if (valorBusca) {
+		resultadoDiv.textContent = `Você buscou: ${valorBusca}`;
+		// fetch(`https://api.github.com/users/${valorBusca}`) 
+        // testContent usar para colocar conteudo em forma de texto
+	} else {
+        resultadoDiv.textContent = "Por favor, insira algo na busca."
+	}
+}
+// evento de click no botao
+botaoBusca.addEventListener("click", (e) =>{
+    searchResult()
+})
